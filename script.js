0f42a67cca0afec41a4cdd41b7e362cec1ec1b04
dzhambov <dzhambov@hotmail.com>
2022-07-07 14:01:44 -0400

    add working progress bar.

--- script.js
+++ script.js
@@ -2,8 +2,43 @@ let counter1 = 0;
 let counter2 = 1;
 
 const sections = document.querySelectorAll('section');
+const progress = document.querySelector('.progress h2');
+const circles = document.querySelectorAll('.circle');
 
-setTimeout(function() {
+const progressCounter = () => {
+  progress.textContent = `${counter2}/${sections.length}`
+
+  Array.from(circles).forEach(circle => {
+    circle.style.backgroundColor = 'transparent';
+  });
+
+  document.querySelector(`.circle-${counter2}`).style.backgroundColor = "#ddd";
+  
+}
+
+const pageController = () => {
+  if (counter1 === 5) {
+    Array.from(sections).forEach((section) => {
+      section.style.left = '0'
+    });
+    counter1 = 0;
+    counter2 = 1;
+    return;
+  }
+  
+  if (counter1 === -1) {
+    Array.from(sections).forEach((section) => {
+      if (section.classList[0] === 'section-5') {
+        return;
+      }
+      section.style.left = "-100vw"
+    })
+    counter1 = 4;
+    counter2 = 5;
+    progressCounter();
+  }
+}
+
   window.addEventListener('wheel', (e) => {
     const deltaY = e.deltaY > 0;
   
@@ -14,29 +49,24 @@ setTimeout(function() {
       counter1--;
       counter2--;
     }
-    
-    if (counter1 === 5) {
-      Array.from(sections).forEach((section) => {
-        section.style.left = '0'
-      });
-      counter1 = 0;
-      counter2 = 1;
-      return;
-    }
-    
-    if (counter1 === -1) {
-      Array.from(sections).forEach((section) => {
-        if (section.classList[0] === 'section-5') {
-          return;
-        }
-        section.style.left = "-100vw"
-      })
-      counter1 = 4;
-      counter2 = 5;
-    }
+
+    pageController()
+    progressCounter()
+    console.log(counter1, counter2);
     
     document.querySelector(`.section-${deltaY ? counter1 : counter2}`).style.left = `${deltaY ? "-100vw" : "0"}`
-  }, 1000);
-    
-  console.log(counter1, counter2);
 });
+
+document.querySelector('.left-btn').addEventListener('click', () => {
+  counter1--;
+  counter2--;
+  document.querySelector(`.section-${counter2}`).style.left = '0';
+  progressCounter();
+})
+
+document.querySelector('.right-btn').addEventListener('click', () => {
+  counter1++;
+  counter2++;
+  document.querySelector(`.section-${counter1}`).style.left = '-100vw';
+  progressCounter();
+})
